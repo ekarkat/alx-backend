262373f9ecd1968426c06bfa1f70c8b178a2eb0d
ekarkat <ekarkat@gmail.com>
2024-04-10 03:34:18 +0100

    task 7

--- 0x02-i18n/7-app.py
+++ 0x02-i18n/7-app.py
@@ -1,1 +1,86 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""A simple flask app"""
+
+
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+import pytz
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config(object):
+    """babel config"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+# def get_locale():
+#     """get local"""
+#     locale = request.args.get('locale')
+#     if locale in app.config['LANGUAGES']:
+#         return locale
+#     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+# app.config.from_pyfile('babel.cfg')
+# babel = Babel(app, locale_selector=get_locale)
+babel = Babel(app)
+
+
+@babel.timezoneselector
+def get_timezone() -> str:
+    """timezone"""
+    try:
+        if request.args.get("timezone"):
+            return pytz.timezone(request.args.get("timezone")).zone
+        if g.user and g.user.get("timezone"):
+            return pytz.timezone(g.user["timezone"]).zone
+    except pytz.exceptions.UnknownTimeZoneError:
+        pass
+    return "UTC"
+
+
+def get_user():
+    """GET user mtd"""
+    user_id = request.args.get('login_as')
+    if user_id:
+        return users.get(int(user_id))
+    return None
+
+
+@app.before_request
+def before_request():
+    """Bfore"""
+    user = get_user()
+    g.user = user
+
+
+@babel.localeselector
+def get_locale():
+    """get local"""
+    locale = request.args.get('locale')
+    if locale in app.config["LANGUAGES"]:
+        return locale
+    if g.user and g.user.get("locale") in app.config["LANGUAGES"]:
+        return g.user["locale"]
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route('/')
+def hello_world():
+    """default route"""
+    return render_template('5-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
